8cbd8ecbaf3dde0d236172489557d32d895e44f5
soulgado <nleonovq@gmail.com>
2025-11-30 08:54:16 +0200

    Fixed errors v2

--- backend/src/middlewares/rateLimit.ts
+++ backend/src/middlewares/rateLimit.ts
@@ -1,8 +1,8 @@
 import { rateLimit } from 'express-rate-limit'
 
 const limiter = rateLimit({
-    windowMs: 1000,
-    limit: 1,
+    windowMs: 60 * 1000,
+    limit: 10,
     standardHeaders: true,
     legacyHeaders: false,
 })
