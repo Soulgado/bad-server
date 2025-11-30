5c139dfc1e2c0c8b038d2cfc70994b43fdf0c4b9
soulgado <nleonovq@gmail.com>
2025-11-30 08:36:47 +0200

    Fixed rate limiter v4

--- backend/src/middlewares/rateLimit.ts
+++ backend/src/middlewares/rateLimit.ts
@@ -2,7 +2,7 @@ import { rateLimit } from 'express-rate-limit'
 
 const limiter = rateLimit({
     windowMs: 1000,
-    limit: 10,
+    limit: 1,
     standardHeaders: true,
     legacyHeaders: false,
 })
